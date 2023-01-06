8ab2a81f999f9ed2ad797aab8be6b15aed5a9097
michaelbruns1 <mb@michaelbruns.one>
2023-01-06 08:28:03 -0800

    page header build complete

--- src/App.js
+++ src/App.js
@@ -20,19 +20,6 @@ function App () {
     <ThemeProvider theme={theme}>
     <div className="App">
       <SiteNavigation />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
     </ThemeProvider>
   );
